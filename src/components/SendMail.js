f55270b14f6612010bde796101c3fd884e59f887
Bhavesh S Singh <bhaveshsinghbiz@gmail.com>
2023-01-29 20:22:02 +0530

    feat:firebase connected, create data works

--- src/components/SendMail.js
+++ src/components/SendMail.js
@@ -1,5 +1,9 @@
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 import { useForm } from "react-hook-form";
 import { GrClose } from "react-icons/gr";
+import { useDispatch } from "react-redux";
+import { closeSendMessage } from "../app/features/messageSlice";
+import { db } from "../firebase";
 
 const SendMail = () => {
   const {
@@ -9,13 +13,25 @@ const SendMail = () => {
     formState: { errors },
   } = useForm();
 
-  const onSubmit = (formData) => {
-    console.log(formData);
+  const onSubmit = async (formData, e) => {
+    await addDoc(collection(db, "message"), {
+      to: formData.to,
+      subject: formData.subject,
+      message: formData.message,
+      timeStamp: serverTimestamp(),
+    });
+
+    dispatch(closeSendMessage());
+  };
+
+  const dispatch = useDispatch();
+  const closeBtnHandler = () => {
+    dispatch(closeSendMessage());
   };
 
   return (
     <div
-      className="w-[500px]  z-50  fixed bottom-0  
+      className="w-[500px]  z-50  fixed bottom-5  
        right-10 border-2 border-slate-800 dark:border-slate-300 rounded-xl
 "
     >
@@ -24,7 +40,9 @@ const SendMail = () => {
       "
       >
         <h3>New Message</h3>
-        <GrClose />
+        <button onClick={closeBtnHandler}>
+          <GrClose />
+        </button>
       </div>
       <form
         onSubmit={handleSubmit(onSubmit)}
@@ -32,7 +50,7 @@ const SendMail = () => {
       >
         <input
           {...register("to", { required: true })}
-          type="text"
+          type="email"
           placeholder="To"
           className="p-2  dark:bg-gray-600 dark:text-gray-100  border-b dark:border-slate-800 border-slate-300"
         />
@@ -90,6 +108,7 @@ const SendMail = () => {
         p-2 rounded-b-lg"
         >
           <button
+            onClick={onSubmit()}
             type="submit"
             className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
           >
